39ddf534f1052996251d135d6c705af6cdceab0e
bessovistnyj <bessovistnyj@gmail.com>
2016-08-14 21:14:06 +0300

    errors correction

---
+++ Arrays/src/main/java/napadovskiyArrays/RotateMatrix.java
@@ -0,0 +1,26 @@
+package napadovskiyArrays;
+
+/**
+	*Class RotateMatrix для поворота массива
+	*@author napadovskiy
+	*@since 08.08.2016
+	*@version 1
+*/
+
+class RotateMatrix {
+	/**
+		*поворот массива
+	*/
+    public int[][] rotateArray(int[][] matrix) {
+		int[][] newMatrix = new int[matrix.length][matrix.length];
+		int x=0,y=0;
+		for(int j=0; j<matrix.length;j++){
+			for(int i = matrix.length-1; i>=0; i--){
+				newMatrix[y][x] = matrix[i][j];
+				x++;
+			}
+		y++;x=0;
+		}
+	return newMatrix;
+	}
+}	
