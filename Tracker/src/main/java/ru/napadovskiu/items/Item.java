3cfa8c8017dd2d2dd162115e654244bbe4a72f82
bessovistnyj <bessovistnyj@gmail.com>
2016-09-15 15:21:11 +0300

    add items and tracker

---
+++ Tracker/src/main/java/ru/napadovskiu/items/Item.java
@@ -0,0 +1,55 @@
+package ru.napadovskiy.item
+
+/**
+ //*Class BubbleSort для сортировки массива
+ *@author napadovskiy
+ *@since 12.09.2016
+ *@version 1
+ */
+
+public  class Item {
+    private String id;
+    private String name;
+  //  private Long creteDate;
+    private String description;
+
+    public Item(){
+
+    }
+
+    public Item(String name, String desc){
+        this.name = name;
+        this.description = desc;
+        // this.creteDate = crateDate;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+
+    public String getId(){
+        return this.id;
+    }
+
+//    public Long getCreateDate(){
+//        return this.creteDate;
+//    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+
---
+++ Tracker/src/main/java/ru/napadovskiu/tracker/Tracker.java
@@ -0,0 +1,97 @@
+package ru.napadovskiy.tracker
+
+import ru.napadodovskiu.items.Item;
+import java.util.Random;
+
+/**
+ * Created by program on 13.09.2016.
+ */
+public class Tracker {
+    private Item[] items = new Item[100];
+    private int position = 0;
+    private static  final Random RN = new Random();
+
+    public  void addNewItem(Item item){
+        if(item.getId() == null){
+            item.setId(generateId());
+        }
+        else{
+            item.setId(item.getId());
+        }
+        this.items[position++] = item;
+    }
+
+    public void deleteItem(Item item){
+        Item[] result = new Item[100];
+
+        int counter =0;
+        for(Item tmpItem: this.items){
+            if(tmpItem!=null && !tmpItem.getId().equals(item.getId())){
+                result[counter] = tmpItem;
+                counter++;
+            }
+        }
+        this.items = result;
+        this.position--;
+    }
+
+
+    public Item[] findItem(String id){
+        Item[] tmpArray = new Item[this.position];
+        int counter =0;
+        for(Item tmpItem: this.items){
+            if(tmpItem.getId().equals(id)){
+                tmpArray[counter] = tmpItem;
+                counter++;
+            }
+        }
+        Item[] result = new Item[counter];
+
+        for(int i =0; i<counter; i++){
+            result[i] = tmpArray[i];
+        }
+        return result;
+    }
+
+    public Item[] findItem(String name,String description){
+        Item[] tmpArray = new Item[this.position];
+        int counter =0;
+        for(Item tmpItem: this.items){
+            if(tmpItem!=null && (tmpItem.getName().equals(name) && tmpItem.getDescription().equals(description))){
+                tmpArray[counter] = tmpItem;
+                counter++;
+            }
+        }
+        Item[] result = new Item[counter];
+
+        for(int i =0; i<counter; i++){
+            result[i] = tmpArray[i];
+        }
+        return result;
+    }
+
+    public void editItem(Item editItem){
+        for (Item tmpItem: this.items){
+            if (tmpItem !=null && tmpItem.getId().equals(editItem.getId())){
+                tmpItem.setName(editItem.getName());
+                tmpItem.setDescription(editItem.getDescription());
+            }
+        }
+    }
+
+
+    public  Item[] showAllItem(){
+        Item[] result = new Item[this.position];
+        for (int counter =0; counter < this.position; counter++){
+            result[counter] = this.items[counter];
+        }
+        return result;
+    }
+
+    private String generateId(){
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt() );
+
+    }
+
+
+}
