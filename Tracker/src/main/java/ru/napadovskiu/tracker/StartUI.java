a83974cb5326e687c3a9616fb971fb13c4b63a92
bessovistnyj <bessovistnyj@gmail.com>
2016-09-21 16:15:27 +0300

    changes 21.09.16

--- Tracker/src/main/java/ru/napadovskiu/tracker/StartUI.java
+++ Tracker/src/main/java/ru/napadovskiu/tracker/StartUI.java
@@ -16,51 +16,94 @@ public class StartUI {
         this.tracker = tracker;
     }
 
-
-
     public void showMenu(){
         System.out.println("1. Add new item ");
         System.out.println("2. Show all item ");
-        System.out.println("3. Find irem by name and description ");
-        System.out.println("4. Edit item ");
-        System.out.println("5. Delete item ");
-        System.out.println("6. Exit");
+        System.out.println("3. Find item by name and description ");
+        System.out.println("4. Find item by id ");
+        System.out.println("5. Edit item ");
+        System.out.println("6. Delete item ");
+        System.out.println("7. Exit");
 
     }
 
-   public void addNewItem(){
+    public void showItem(Item[] arrayItem){
+       //
+       Integer counter =1;
+        for(Item tmpItem : arrayItem){
 
-       String itemName = this.input.ask("please enter the name of task");
-       String itemDescription = this.input.ask("please enter the description of task");
-
+           String itemId    = tmpItem.getId();
+           String itemName  = tmpItem.getName();
+           String itemDesc  = tmpItem.getDescription();
+           System.out.println(counter+". "+itemId+" "+ itemName +" "+itemDesc+" ");
+           counter++;
+       }
    }
+
+    private Item takeItemFromArray(Item[] arrayItem){
+       String counter = this.input.ask("please enter number delete item ");
+       Integer i = new Integer(counter);
+       return arrayItem[i-1];
+   }
+
+    public Tracker getTracker(){
+        return this.tracker;
+    }
 
     public void init(){
         boolean exit =false;
             this.showMenu();
-//        do {
-            //Integer userChoice = this.input.userChoice("Please make your choice");
-//           if (userChoice.intValue() == 1){
-//               this.addNewItem();// System.out.println("add new item");
-//           }
-//        } while(!exit );
+        do {
+            String  action = this.input.ask("Choice your action");
+            if (action.equals("1")){
+                this.tracker.addNewItem(new Item(this.input.ask("please enter the name of item"),this.input.ask("please enter the description of item")));
+            }
+            if (action.equals("2")){
+                Item[] arrayItem  = this.tracker.showAllItem();
+                this.showItem(arrayItem);
+            }
+            if (action.equals("3")){
+                Item[] findItem = this.tracker.findItem(this.input.ask("please enter the name of item"),this.input.ask("please enter the description of item"));
+                this.showItem(findItem);
+            }
+            if (action.equals("4")){
+                Item[] findItem = this.tracker.findItem(this.input.ask("please enter id Item"));
+                this.showItem(findItem);
+            }
+            if (action.equals("5")){
+                Item[] findItem = this.tracker.findItem(this.input.ask("please enter id Item"));
+                Item editItem = new Item(this.input.ask("please enter the name of item"),this.input.ask("please enter the description of item"));
+                if (findItem.length > 0 && findItem.length == 1){
+                    editItem.setId(findItem[0].getId());
+                    this.tracker.editItem(editItem);
+                }else{
+                    editItem.setId(takeItemFromArray(findItem).getId());
+                    this.tracker.editItem(editItem);
+                }
+            }
+            if (action.equals("6")){
+                Item[] findItem = this.tracker.findItem(this.input.ask("please enter id Item"));
+                if (findItem.length > 0 && findItem.length == 1){
+                    this.tracker.deleteItem(findItem[0]);
+                }else{
+                    this.tracker.deleteItem(takeItemFromArray(findItem));
+                }
+            }
+            if (action.equals("7")){
+                exit = true;
+            }
+        } while(!exit );
 
     }
-
-
 
     public static void main(String[] args){
         Input consoleInput = new ConsoleInput();
         Tracker tracker = new Tracker();
         new StartUI(consoleInput,tracker).init();
-//
+//        String[] params = new String[]{"1","Task id","Task name","Task desc","1","Task id","","","7"};
 //        Tracker tracker = new Tracker();
-//        tracker.addNewItem(new Item(name,"firstItemDescription"));
-//        for (Item item: tracker.showAllItem()){
-//            System.out.println(item.getName());
-//        }
-
-
+//        Input input = new StubInput(params);
+//        new StartUI(input,tracker).init();
     }
 
 }
---
+++ Tracker/src/main/java/ru/napadovskiu/tracker/StubInput.java
@@ -0,0 +1,16 @@
+package ru.napadovskiu.tracker;
+
+public class StubInput implements  Input{
+
+    private String[] answers;
+    private  int position = 0;
+
+    public  StubInput(String[] answers){
+        this.answers = answers;
+    }
+
+    public String ask(String question){
+        return answers[this.position++];
+    }
+
+}
