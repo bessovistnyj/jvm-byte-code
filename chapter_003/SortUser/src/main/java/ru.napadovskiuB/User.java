ff11a612fd86e100a74f776829ac5d8b23863d2b
bessovistnyj <bessovistnyj@gmail.com>
2017-06-14 18:46:37 +0300

    correction mistake

--- chapter_003/SortUser/src/main/java/ru.napadovskiuB/User.java
+++ chapter_003/SortUser/src/main/java/ru.napadovskiuB/User.java
@@ -11,18 +11,37 @@ public class User implements Comparable<User> {
     private int age;
 
     /**
+     * User id.
+     */
+    private int id;
+
+    /**
      * User name.
      */
     private String name;
 
+
     /**
      * Constructor for User class.
-     * @param age user id.
+     * @param id user id.
      * @param name user name.
      */
-    public User(int age, String name) {
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Constructor for User class.
+     * @param id user id.
+     * @param age user age.
+     * @param name user name.
+     */
+    public  User(int id, int age, String name) {
+        this.id = id;
         this.age = age;
         this.name = name;
+
     }
 
     /**
@@ -34,11 +53,19 @@ public class User implements Comparable<User> {
     }
 
     /**
-     *Method return user age.
+     *Method return user id.
      * @return user age.
      */
     public int getAge() {
         return this.age;
+    }
+
+    /**
+     *Method return user id.
+     * @return user id.
+     */
+    public int getId() {
+        return  this.id;
     }
 
 
