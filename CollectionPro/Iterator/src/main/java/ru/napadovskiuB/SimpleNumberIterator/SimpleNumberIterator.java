4598973a6585df5fefe374e5d98712c8f9335dc0
bessovistnyj <bessovistnyj@gmail.com>
2017-07-01 17:55:19 +0300

    5.1.3. Создать итератор простые числа.[#17728]

--- CollectionPro/Iterator/src/main/java/ru/napadovskiuB/SimpleNumberIterator/SimpleNumberIterator.java
+++ CollectionPro/Iterator/src/main/java/ru/napadovskiuB/SimpleNumberIterator/SimpleNumberIterator.java
@@ -11,33 +11,80 @@ import java.util.Iterator;
  */
 public class SimpleNumberIterator implements Iterator {
 
+    /**
+     * Main array.
+     */
     private int[] array;
 
+    /**
+     * array cursor.
+     */
     private int cursor = 0;
 
-    public SimpleNumberIterator (int[] array) {
+    /**
+     *Constructor for ArrayIterator class.
+     * @param array main array.
+     */
+    public SimpleNumberIterator(int[] array) {
         this.array = array;
 
     }
 
+    /**
+     * Method checks number is prime.
+     * @param numberForCheck number for check.
+     * @return result.
+     */
+    private boolean checkNumber(int numberForCheck) {
+        boolean result = true;
+        if (numberForCheck >= 2) {
+            for (int i = 2; i * i <= numberForCheck; i++) {
+                if (numberForCheck % i == 0) {
+                    result = false;
+                }
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    /**
+     *Method checks the prime number and return position.
+     * @return position.
+     */
     private int checkSimpleNumber() {
         int position = this.cursor;
         for (; position < this.array.length; position++) {
-            if ((this.array[position]%this.array[position] ==0) | (this.array[position]%1 ==0)) {
+            if (checkNumber(this.array[position])) {
                 return position;
             }
         }
-        return -1;
+        return  -1;
     }
 
 
+    /**
+     *Method check can move in array.
+     * @return result.
+     */
     @Override
     public boolean hasNext() {
-        return false;
+        boolean result = false;
+        if (checkSimpleNumber() != -1) {
+            result = true;
+        }
+
+        return result;
     }
 
+    /**
+     * Method take next value from array.
+     * @return result.
+     */
     @Override
     public Object next() {
-        return null;
+        this.cursor = checkSimpleNumber();
+        return  this.array[this.cursor++];
     }
 }
