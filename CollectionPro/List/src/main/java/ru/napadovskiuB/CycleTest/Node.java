9fa9973978a7f748df1855a572c5fcb2d22d0e94
bessovistnyj <bessovistnyj@gmail.com>
2017-07-11 10:26:54 +0300

    add 5.3.4. Задан связанный список. Определить цикличность.

--- CollectionPro/List/src/main/java/ru/napadovskiuB/CycleTest/Node.java
+++ CollectionPro/List/src/main/java/ru/napadovskiuB/CycleTest/Node.java
@@ -1,38 +1,67 @@
 package ru.napadovskiuB.CycleTest;
 
 /**
- * Created by Владелец on 10.07.2017.
+ * Package of CollectionPro testTask.
+ *
+ * @author Napadovskiy Bohdan
+ * @version 1.0
+ * @since 11.07.2017
+ * @param <T> generic.
  */
 public class Node<T> {
 
+    /**
+     * value of node.
+     */
     private T value;
 
+    /**
+     * next node.
+     */
     private Node<T> next;
 
-    public Node (T value) {
+    /**
+     *Constructor for class with value.
+     * @param value for node.
+     */
+    public Node(T value) {
         this.value = value;
         this.next = null;
     }
 
+    /**
+     * Method return next value.
+     * @return next value.
+     */
     public T getNext() {
         return (T) this.next;
     }
 
+    /**
+     *Method set next value.
+     * @param nextValue next value.
+     */
     public void setNext(T nextValue) {
         this.next = (Node<T>) nextValue;
 
     }
 
+    /**
+     *Method for check the cycle.
+     * @param first first value.
+     * @return result.
+     */
     boolean hasCycle(Node first) {
         boolean result = false;
-        while (first.next != null) {
-            Node tmpValue = first.next;
+        Node tmpValue = first;
+        while (tmpValue.next != null) {
             if (tmpValue.next.equals(first) || tmpValue.next.equals(tmpValue)) {
                 result = true;
+                break;
             }
+            tmpValue = tmpValue.next;
         }
         return result;
     }
-
 
 }
--- CollectionPro/List/src/test/java/ru/napadovskiuB/CycleTest/NodeTest.java
+++ CollectionPro/List/src/test/java/ru/napadovskiuB/CycleTest/NodeTest.java
@@ -2,26 +2,63 @@ package ru.napadovskiuB.CycleTest;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Владелец on 10.07.2017.
  */
 public class NodeTest {
 
+    /**
+     *
+     */
+    private final Node first = new Node(1);
+
+    /**
+     *
+     */
+    private final Node two = new Node(2);
+
+    /**
+     *
+     */
+    private final Node third = new Node(3);
+
+    /**
+     *
+     */
+    private final Node four = new Node(4);
+
+
+    /**
+     *
+     */
     @Test
-    public void TestNode() {
-        Node first = new Node(1);
-        Node two = new Node(2);
-        Node third = new Node(3);
-        Node four = new Node(4);
+    public void whenListHasCycleThenReturnTrue() {
 
         first.setNext(two);
         two.setNext(third);
         third.setNext(four);
-        four.setNext(first) ;
+        four.setNext(first);
         boolean result = first.hasCycle(first);
-        int a =1;
+
+        assertThat(result, is(true));
+
+    }
+
+    /**
+     *
+     */
+    @Test
+
+    public void whenListHasntCycleThenReturnFalse() {
+        first.setNext(two);
+        two.setNext(third);
+        third.setNext(four);
+        boolean result = first.hasCycle(first);
+
+        assertThat(result, is(false));
 
     }
 
