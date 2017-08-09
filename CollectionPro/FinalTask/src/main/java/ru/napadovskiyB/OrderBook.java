abd34b6da8832367ec8907b1030e53c315df51b1
bessovistnyj <bessovistnyj@gmail.com>
2017-08-09 19:26:33 +0300

    work with final task

--- CollectionPro/FinalTask/src/main/java/ru/napadovskiyB/OrderBook.java
+++ CollectionPro/FinalTask/src/main/java/ru/napadovskiyB/OrderBook.java
@@ -79,9 +79,72 @@ public class OrderBook {
             this.unsortedMap.remove(id);
             if (this.buyTree.containsKey(deleteOrder.getPrice())) {
                 this.buyTree.get(deleteOrder.getPrice()).remove(deleteOrder.getIdOrder());
+                if (this.buyTree.get(deleteOrder.getPrice()).size() == 0) {
+                    this.buyTree.remove(deleteOrder.getPrice());
+                }
             }
             if (this.sellTree.containsKey(deleteOrder.getPrice())) {
                 this.sellTree.get(deleteOrder.getPrice()).remove(deleteOrder.getIdOrder());
+                if (this.sellTree.get(deleteOrder.getPrice()).size() == 0) {
+                    this.sellTree.remove(deleteOrder.getPrice());
+                }
+
+            }
+        }
+    }
+
+
+    private int takeSummAllEllemnt (HashMap<Integer,Order> mapForCheck) {
+        int result = 0;
+        for (Order tmpOrder:mapForCheck.values()) {
+            result = result + tmpOrder.getVolume();
+        }
+        return result;
+    }
+
+    private void calculateVolume(HashMap<Integer,Order> editMap,int value) {
+        Iterator<Order> iterMap = editMap.values().iterator();
+        while (iterMap.hasNext()) {
+            Order nextOrder = iterMap.next();
+            if (value <= nextOrder.getVolume()) {
+                nextOrder.setVolume(nextOrder.getVolume()-value);
+                break;
+            } else {
+                iterMap.remove();
+            }
+        }
+
+    }
+
+
+    public void checkElement() {
+        Iterator<Double> buyKeys = this.buyTree.keySet().iterator();
+        Iterator<Double> sellKeys = this.sellTree.keySet().iterator();
+        if (buyKeys.hasNext() && sellKeys.hasNext()) {
+            Double buyPrice = buyKeys.next();
+            Double sellPrice = sellKeys.next();
+            while(buyKeys.hasNext() && sellKeys.hasNext()) {
+                HashMap buyElements = this.buyTree.get(buyPrice);
+                HashMap sellElements = this.sellTree.get(sellPrice);
+                int sumAllBuyElement = takeSummAllEllemnt(buyElements);
+                int sumAllSellElement = takeSummAllEllemnt(sellElements);
+                if ((sellPrice >= buyPrice) && (sumAllBuyElement > sumAllSellElement)) {
+                    calculateVolume(buyElements,sumAllSellElement) ;
+                    sellKeys.remove();
+                    sellPrice = sellKeys.next();
+                } else if ((sellPrice >= buyPrice) && (sumAllBuyElement < sumAllSellElement)) {
+                    calculateVolume(sellElements,sumAllBuyElement) ;
+                    buyKeys.remove();
+                    buyPrice = buyKeys.next();
+                } else if ((sellPrice >= buyPrice) && (sumAllBuyElement == sumAllSellElement)) {
+                    sellKeys.remove();
+                    sellPrice = sellKeys.next();
+                    buyKeys.remove();
+                    buyPrice = buyKeys.next();
+                } else {
+                    break;
+                }
+
             }
         }
     }
