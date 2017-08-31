caea29725aaf8ea5fcd1b4cc9a2d0c9952c5afb7
bessovistnyj <bessovistnyj@gmail.com>
2017-08-31 18:28:02 +0300

    1. Проиллюстрировать проблемы с многопоточностью.

---
+++ Multithreading/JMM/src/main/java/ru.Napadovskiy/raceCondition/RaceCondition.java
@@ -0,0 +1,54 @@
+package ru.Napadovskiy.raceCondition;
+
+/**
+ * Package of Multithreading.
+ * @author Napadovskiy Bohdan
+ * @version 1.0
+ * @since 31.08.2017
+ */
+public class RaceCondition implements Runnable {
+
+    /**
+     * Counter.
+     */
+    private int count;
+
+    /**
+     * Method add counter.
+     * @param amount amount.
+     */
+    public void addToCount(int amount) {
+        this.count += amount;
+
+    }
+
+    /**
+     * Method return counter.
+     * @return count.
+     */
+    public int getCount() {
+        return count;
+    }
+
+
+    /**
+     * Method return.
+     */
+    @Override
+    public void run() {
+
+        final int checkCount = 2000;
+        final int maxI = 10;
+
+        while (!Thread.currentThread().isInterrupted() && this.count < checkCount) {
+            for (int i = 0; i < maxI; i++) {
+                this.addToCount((1));
+            }
+            try {
+                Thread.currentThread().sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Multithreading/JMM/src/main/java/ru.Napadovskiy/raceCondition/TestRaceCondition.java
@@ -0,0 +1,37 @@
+package ru.Napadovskiy.raceCondition;
+
+/**
+ * Package of Multithreading.
+ * Main class for test.
+ * @author Napadovskiy Bohdan
+ * @version 1.0
+ * @since 31.08.2017
+ */
+public class TestRaceCondition {
+
+    /**
+     * Main method for check.
+     * @param args array string.
+     */
+    public static void main(String[] args) {
+        RaceCondition raceCondition = new RaceCondition();
+        Thread firstThread = new Thread(raceCondition);
+        Thread secondThread = new Thread(raceCondition);
+
+        firstThread.start();
+        secondThread.start();
+
+        try {
+            firstThread.join();
+            secondThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        firstThread.interrupt();
+        secondThread.interrupt();
+
+        System.out.println("All thread finish " + raceCondition.getCount());
+
+    }
+}
