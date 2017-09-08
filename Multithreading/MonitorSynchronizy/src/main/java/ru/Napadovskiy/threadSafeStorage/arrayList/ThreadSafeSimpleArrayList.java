a74ef6f206e7dd09c762f46efd2c98def0999dd6
bessovistnyj <bessovistnyj@gmail.com>
2017-09-08 16:36:51 +0300

    3. ThreadSafe динамический список [#23571]

---
+++ Multithreading/MonitorSynchronizy/src/main/java/ru/Napadovskiy/threadSafeStorage/arrayList/ThreadSafeSimpleArrayList.java
@@ -0,0 +1,145 @@
+package ru.Napadovskiy.threadSafeStorage.arrayList;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import ru.Napadovskiy.threadSafeStorage.SimpleContainer;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Napadovskiy Bohdan
+ * @version 1.0
+ * @since 05.06.2017
+ * @param <E> generic.
+ */
+@ThreadSafe
+public class ThreadSafeSimpleArrayList<E> implements SimpleContainer<E> {
+
+    /**
+     *Main array.
+     */
+    @GuardedBy("this")
+    private Object[] mainArray;
+
+    /**
+     * Default size array.
+     */
+    private final int defaultSize = 2;
+
+    /**
+     * current index.
+     */
+    @GuardedBy("this")
+    private int currentIndex;
+
+    /**
+     * index too add element.
+     */
+    @GuardedBy("this")
+    private int indexForWrite;
+
+
+    /**
+     *Default constructor for array.
+     */
+    public ThreadSafeSimpleArrayList() {
+        this.mainArray = new Object[defaultSize];
+        this.currentIndex = 0;
+        this.indexForWrite = 0;
+
+    }
+
+    /**
+     *Constructor with size.
+     * @param size array size.
+     */
+    public ThreadSafeSimpleArrayList(int size) {
+        this.mainArray = new Object[size];
+        this.currentIndex = 0;
+    }
+
+    /**
+     * Method to resize array if needed.
+     */
+    private void resizeArray() {
+       synchronized (this) {
+           int newCapacity = (this.mainArray.length * 2);
+           this.mainArray = Arrays.copyOf(this.mainArray, newCapacity);
+
+       }
+    }
+
+    /**
+     *Method add value too array.
+     * @param value
+     */
+    @Override
+    public void add(E value) {
+        synchronized (this) {
+            if (this.indexForWrite == (this.mainArray.length)) {
+                resizeArray();
+            }
+            this.mainArray[this.indexForWrite++] = value;
+        }
+    }
+
+    /**
+     * Method return value by index.
+     * @param index for search.
+     * @return value.
+     * @throws NullPointerException exception.
+     */
+    @Override
+    public E get(int index) throws NullPointerException {
+        synchronized (this) {
+            if (index < this.mainArray.length) {
+                this.currentIndex = index;
+                return (E) this.mainArray[index];
+
+            }
+            throw new NullPointerException();
+        }
+    }
+
+
+    /**
+     *Method for iterator.
+     * @return iterator.
+     */
+    @Override
+    public Iterator iterator() {
+
+        return new Iterator() {
+
+            /**
+             * method check can we move next.
+             * @return result.
+             */
+            @Override
+            public boolean hasNext() {
+                return currentIndex < (mainArray.length - 1);
+            }
+
+            /**
+             * Method return current value if we can move.
+             * @return value.
+             * @throws NoSuchElementException exception.
+             */
+            @Override
+            public Object next() throws NoSuchElementException {
+                if (hasNext()) {
+                    int current = currentIndex;
+                    currentIndex++;
+                    return mainArray[current];
+                }
+                throw new NoSuchElementException();
+
+            }
+        };
+    }
+
+
+
+}
---
+++ Multithreading/MonitorSynchronizy/src/main/java/ru/Napadovskiy/threadSafeStorage/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package of CollectionPro testTask.
+ *
+ * @author Napadovskiy Bohdan
+ * @version 1.0
+ * @since 08.09.2017
+ */
+package ru.Napadovskiy.threadSafeStorage;
