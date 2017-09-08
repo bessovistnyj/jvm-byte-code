879dce75c6d4c3ea0b9bcbf92d1a037b52b1ded0
bessovistnyj <bessovistnyj@gmail.com>
2017-09-08 16:29:42 +0300

    1. класс Count и метод int increma[#23569]

---
+++ Multithreading/MonitorSynchronizy/src/main/java/ru/Napadovskiy/Count.java
@@ -0,0 +1,43 @@
+package ru.Napadovskiy;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * Package of Multithreading treads.
+ * Class counter.
+ * @author Napadovskiy Bohdan
+ * @version 1.0
+ * @since 04.09.2017
+ */
+
+@ThreadSafe
+public class Count {
+
+
+    /**
+     * counter.
+     */
+    @GuardedBy("this")
+    private int counter;
+
+    /**
+     * Method to increment counter.
+     */
+    public void increment() {
+        synchronized (this) {
+            this.counter++;
+        }
+    }
+
+    /**
+     * Method return counter.
+     * @return counter.
+     */
+    public int getCounter() {
+        synchronized (this) {
+            return  this.counter;
+        }
+    }
+
+}
---
+++ Multithreading/MonitorSynchronizy/src/main/java/ru/Napadovskiy/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package of Multithreading treads.
+ *
+ * @author Napadovskiy Bohdan
+ * @version 1.0
+ * @since 31.08.2017
+ */
+package ru.Napadovskiy;
