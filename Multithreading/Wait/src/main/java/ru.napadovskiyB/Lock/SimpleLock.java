a4322d26eb031ba063c00fb05f133b51dc63e994
bessovistnyj <bessovistnyj@gmail.com>
2017-09-26 15:32:11 +0300

    3. Реализовать собственный механизм блокировок Lock.[#23867]

---
+++ Multithreading/Wait/src/main/java/ru.napadovskiyB/Lock/SimpleLock.java
@@ -0,0 +1,36 @@
+package ru.napadovskiyB.Lock;
+
+/**
+ * Package of  of Multithreading treads.
+ *
+ * @author Napadovskiy Bohdan
+ * @version 1.0
+ * @since 11.09.2017
+ */
+public class SimpleLock {
+
+    /**
+     * flag for check lock.
+     */
+    private boolean isLocked = false;
+
+    /**
+     * Method for lock.
+     * @throws InterruptedException exception.
+     */
+    public synchronized void lock() throws InterruptedException {
+        while (isLocked) {
+            wait();
+        }
+        isLocked = true;
+    }
+
+    /**
+     * Method for unlock.
+     */
+    public synchronized void unlock() {
+        isLocked = false;
+        notifyAll();
+    }
+
+}
---
+++ Multithreading/Wait/src/main/java/ru.napadovskiyB/Lock/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package of Multithreading treads.
+ *
+ * @author Napadovskiy Bohdan
+ * @version 1.0
+ * @since 11.09.2017
+ */
+package ru.napadovskiyB.Lock;
