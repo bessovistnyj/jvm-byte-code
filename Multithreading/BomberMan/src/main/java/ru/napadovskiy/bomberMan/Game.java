a6ceffd875143d55b1970173d320c2221ddd1e1e
bessovistnyj <bessovistnyj@gmail.com>
2017-11-20 19:05:11 +0200

    work with test task

--- Multithreading/BomberMan/src/main/java/ru/napadovskiy/bomberMan/Game.java
+++ Multithreading/BomberMan/src/main/java/ru/napadovskiy/bomberMan/Game.java
@@ -1,5 +1,8 @@
 package ru.napadovskiy.bomberMan;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Package of Multithreading.
  * Main class for test.
@@ -9,32 +12,66 @@ package ru.napadovskiy.bomberMan;
  */
 public class Game {
 
+
+    private final int countOfMonsters = 5;
+
+    private final int xSizeBoard = 8;
+
+    private final int ySizeBoard = 8;
+
+    private  final GameBoard board;
+
+
+    private static ExecutorService service = Executors.newCachedThreadPool();
+
+    private Game(int xSize, int ySize) {
+        this.board = new GameBoard(xSize, ySize);
+
+    }
+
+    private void addHero() {
+
+        if (this.board.getBoard()[0][0] != null) {
+            if (this.board.getBoard()[0][0].tryLock()) {
+                Hero hero = new Hero(0, 0, board, service);
+            }
+        } else {
+            Hero hero = new Hero(0, 0, board, service);
+        }
+    }
+
+    private void addMonster() {
+        if (this.board.getBoard()[3][3] != null) {
+            if (this.board.getBoard()[3][3].tryLock()) {
+                Monster monster = new Monster(3,3, board,service);
+            }
+        } else {
+            Monster monster = new Monster(3,3, board,service);
+        }
+    }
+    private void initGame() {
+        addHero();
+        addMonster();
+
+    }
+
+
+
     /**
      * Main method for game.
      * @param args array string.
      */
     public static void main(String[] args) {
 
-        Game game = new Game();
+        Game game = new Game(8, 8);
+        game.initGame();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
 
-        final int xSize = 8;
-
-        final int ySize = 8;
-
-        final int countOfMonsters = 5;
-
-
-        GameBoard gameBoard = new GameBoard(xSize, ySize);
-
-
-//        for (int i =0; i < countOfMonsters; i++) {
-//
-//            //new Thread(new Monster(gameBoard).setUnitInPosition()).start();
-//        }
-//
-//        Thread heroThread = new Thread(hero);
-//        heroThread.start();
-//        heroThread.interrupt();
     }
 
 }
