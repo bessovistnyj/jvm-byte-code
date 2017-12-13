a19f3b7c2fa073c78d785de391745d800ebe9cb5
bessovistnyj <bessovistnyj@gmail.com>
2017-12-13 14:39:10 +0200

    1. UML системы

---
+++ SQL_JDBC/1.Create_Update_Insert/CreateTable.sql
@@ -0,0 +1,59 @@
+﻿
+CREATE TABLE table_Role (
+role_Id SERIAL PRIMARY KEY,
+role_Name VARCHAR(100)
+);
+
+CREATE TABLE table_user (
+user_Id SERIAL PRIMARY KEY,
+user_Login varchar(50) not null,
+user_Pasword varchar(50),
+role_Id integer references table_Role(role_Id)  
+);
+
+
+CREATE TABLE table_Rules (
+rules_Id SERIAL PRIMARY KEY,
+rules_Name VARCHAR(100) 
+);
+
+CREATE TABLE table_Rules_to_Role (
+
+rules_Id integer references table_Rules(rules_Id) ,
+role_Id integer references table_Role(role_Id) 
+);
+
+CREATE TABLE table_State (
+state_id SERIAL PRIMARY KEY,
+state_desc VARCHAR(50)
+);
+
+CREATE TABLE table_Category (
+Category_id SERIAL PRIMARY KEY,
+Category_desc VARCHAR(50)
+); 
+
+CREATE TABLE table_Item (
+item_Id SERIAL PRIMARY KEY UNIQUE,
+item_Desc VARCHAR(200),
+user_id integer references table_user(user_id) UNIQUE,
+state_id integer references table_State(state_id),
+category_id integer references table_Category(Category_id)
+);
+
+CREATE TABLE table_Comments (
+comments_id SERIAL PRIMARY KEY,
+comments_Desc VARCHAR(200),
+item_id integer references table_Item(item_Id)
+);
+
+
+CREATE TABLE table_Attachs (
+attachs_id SERIAL PRIMARY KEY,
+attach_date timestamp,
+file_name VARCHAR(255),
+item_id integer references table_Item(item_Id)
+);
+
+
+
---
+++ SQL_JDBC/1.Create_Update_Insert/InsertInToTable.sql
@@ -0,0 +1,41 @@
+﻿INSERT INTO table_role (role_id, role_name) VALUES
+	(1,'Root role'),
+	(2,'user role');
+	
+INSERT INTO table_rules (rules_id, rules_name) VALUES
+    (1,'Root rules'),
+    (2,'user rules');
+
+INSERT INTO table_rules_to_role (rules_id, role_id) VALUES
+       (1,1),
+       (1,2),
+       (2,1);
+
+
+INSERT INTO table_user (user_login, user_pasword, role_id) VALUES
+      ('root','paswordRoot',1),
+      ('User1','paswordUser1',2),
+      ('User2', 'PaswordUser2',2);
+
+
+INSERT INTO table_state (state_id, state_desc) VALUES
+	(1,'заявка в работе'),
+	(2,'заявка закрыта');
+
+
+INSERT INTO table_category (category_id, category_desc) VALUES
+	(1,'заявка по ремонту техники'),
+	(2,'заявка на установку');
+
+
+INSERT INTO table_item (item_desc, user_id, state_id,category_id) VALUES
+	('Заявка № 1',1,2,2),
+	('Заявка № 2',2,2,1);
+
+INSERT INTO table_attachs (attach_date, file_name, item_id) VALUES
+	('2017-12-11 09:15:36','picture.jpg',1),
+	('2017-12-11 08:00:00','masage.txt',2);
+
+INSERT INTO table_comments (comments_desc, item_id) VALUES
+	('замечания к заявке',1),
+	('',2);
