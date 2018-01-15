a483cf4d653e840d981d3c5253c48b3904c492be
bessovistnyj <bessovistnyj@gmail.com>
2018-01-15 19:10:42 +0200

    Дополнительное задание[#37389]

---
+++ SQL_JDBC/5.FinalTask/query/DopTask.sql
@@ -0,0 +1,9 @@
+﻿SELECT person.id, person.name, company.id, company.name FROM person AS person 
+LEFT JOIN company ON person.company_id = company.id
+WHERE person.company_id <> 5;
+
+SELECT company.id, company.name, COUNT(person.name)as person_count, MAX(person.id)
+FROM company AS company
+LEFT JOIN person AS person ON (company.id = person.company_id)
+GROUP BY company.name,company.id
+ORDER BY company.id;
