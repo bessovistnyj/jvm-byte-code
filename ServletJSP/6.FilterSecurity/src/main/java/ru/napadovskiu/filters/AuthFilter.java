8e6209769d2e591317a867b050a1fa050fc84f8e
bessovistnyj <bessovistnyj@gmail.com>
2018-05-12 19:10:47 +0300

    1. Реализовать авторизация и аутентификацию

---
+++ ServletJSP/6.FilterSecurity/src/main/java/ru/napadovskiu/filters/AuthFilter.java
@@ -0,0 +1,60 @@
+package ru.napadovskiu.filters;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+    /**
+     *
+     * @param filterConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    /***
+     *
+     * @param servletRequest
+     * @param servletResponse
+     * @param filterChain
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpSession session = request.getSession();
+        if (request.getRequestURI().contains("/login")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            synchronized (session) {
+                if (session.getAttribute("login") == null) {
+                    ((HttpServletResponse) servletResponse).sendRedirect(String.format("%s/login", request.getContextPath()));
+                    return;
+                }
+            }
+            request.setAttribute("userRole", request.getParameter("userRole"));
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void destroy() {
+
+    }
+}
